dfb4a53891ac7f58cbbc01cf76693eefe7f2e3cb
5jisoo <56earls@gmail.com>
2023-06-28 22:31:54 +0900

    initial setting

---
+++ info-dustdrive/info-dustdrive/src/main/java/com/dustdrive/infodustdrive/InfoDustdriveApplication.java
@@ -0,0 +1,13 @@
+package com.dustdrive.infodustdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoDustdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoDustdriveApplication.class, args);
+	}
+
+}
---
+++ info-dustdrive/info-dustdrive/src/test/java/com/dustdrive/infodustdrive/InfoDustdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustdrive.infodustdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoDustdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
